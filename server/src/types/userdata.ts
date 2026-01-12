59d2262c854b58c61b0d6ed9749aa67c74acd2f7
Király Sándor <kiralysandor1986@gmail.com>
2026-01-12 14:06:10 +0100

    update userdata type

--- server/src/types/userdata.ts
+++ server/src/types/userdata.ts
@@ -3,7 +3,8 @@ interface userdata {
     username: string,
     password?: string,
     pubKey: string,
-    encryptedPrivKey?: string
+    encryptedPrivKey?: string,
+    lastKeyRotation? : Date,
     created_at: Date,
     updated_at: Date
 }
