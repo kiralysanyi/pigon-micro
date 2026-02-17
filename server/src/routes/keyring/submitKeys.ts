a5cbb0accd0e0211480e38fb550ee0d2c13b2c9f
Király Sándor <kiralysandor1986@gmail.com>
2026-02-17 08:24:42 +0100

    verify access in key upload

--- server/src/routes/keyring/submitKeys.ts
+++ server/src/routes/keyring/submitKeys.ts
@@ -1,10 +1,10 @@
 import { RequestHandler } from "express";
 import { reqWithUserinfo } from "../../types/reqWithUserinfo";
 import { validationResult } from "express-validator";
-import { getParticipants } from "../../utils/db/chat";
+import { checkUserInChat, getParticipants } from "../../utils/db/chat";
 import { addChatKey } from "../../utils/db/keyring";
 
-const submitKeys: RequestHandler = (req: reqWithUserinfo, res) => {
+const submitKeys: RequestHandler = async (req: reqWithUserinfo, res) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.status(400).json({
@@ -17,6 +17,24 @@ const submitKeys: RequestHandler = (req: reqWithUserinfo, res) => {
 
     switch (type) {
         case "chat":
+            // check if user in chat
+
+            try {
+                const inChat = await checkUserInChat(req.userinfo.ID, chatID);
+                if (inChat != true) {
+                    return res.status(403).json({
+                        success: false,
+                        message: "You are not allowed to add keys to this chat"
+                    })
+                }
+            } catch (error) {
+                console.error(error);
+                return res.status(500).json({
+                    success: false,
+                    message: "Failed to verify permissions"
+                })
+            }
+
             getParticipants(chatID).then(async (participants) => {
                 for (let i in participants) {
                     await addChatKey(participants[i].ID, chatID, key)
--- server/src/utils/db/chat.ts
+++ server/src/utils/db/chat.ts
@@ -9,7 +9,6 @@ const getParticipants = (chatID: number): Promise<QueryResult> => {
         WHERE \`user-chats\`.chatId = ?;`;
         pool.query(sql, [chatID], (err, result) => {
             if (err) {
-                ;
                 console.error("Failed to get participants: ", err)
                 return rejected(err)
             }
@@ -19,4 +18,23 @@ const getParticipants = (chatID: number): Promise<QueryResult> => {
     })
 }
 
-export { getParticipants };
+
+const checkUserInChat = (userId: number, chatId: number): Promise<boolean> => {
+    return new Promise((resolved, rejected) => {
+        const sql = `SELECT 1 FROM \`user-chats\` WHERE userId = ? AND chatId = ?`
+        pool.query(sql, [userId, chatId], (err, result) => {
+            if (err) {
+                console.error("Failed to check user in chat: ", err)
+                return rejected(err)
+            }
+
+            if (result[0] != undefined) {
+                resolved(true)
+            } else {
+                resolved(false)
+            }
+        })
+    });
+}
+
+export { getParticipants, checkUserInChat };
