60b994b03e54059a40ac1f17f09eac8bb7f40dbd
Király Sándor <kiralysandor1986@gmail.com>
2026-03-15 18:23:01 +0100

    fix getAccessToken going crazy under multiple parallel calls

--- pigon-micro-web/src/lib/auth/getAccessToken.ts
+++ pigon-micro-web/src/lib/auth/getAccessToken.ts
@@ -1,36 +1,62 @@
 import refreshAccessToken from "./refreshAccessToken";
 
 // todo: fix expired token handling
+let lock = false;
+
 const getAccessToken = (): Promise<string> => {
     return new Promise(async (resolved, rejected) => {
-        let atoken = localStorage.getItem("atoken");
-        let atokenExpire = localStorage.getItem("atokenExpire");
-        if (atoken == null || atokenExpire == null) {
-            console.error("No access token found");
-            rejected();
-            return;
-        }
-
-        const expireDate = new Date(atokenExpire);
-        const expired = new Date() > expireDate;
-        const diffMs = Math.abs(expireDate.getTime() - new Date().getTime());
-        const diffMinutes = diffMs / (1000 * 60);
-        const nearExpire = diffMinutes < 2;
-
-        if (expired || nearExpire) {
-            try {
-                const newToken = await refreshAccessToken();
-                atoken = newToken.token;
-                localStorage.setItem("atoken", newToken.token);
-                localStorage.setItem("atokenExpire", newToken.tokenExpire);
-                resolved(atoken);
-            } catch (error) {
-                console.error("Failed to get new access token: ", error)
+        // actual logic for getting token
+        const getToken = async () => {
+            let atoken = localStorage.getItem("atoken");
+            let atokenExpire = localStorage.getItem("atokenExpire");
+            if (atoken == null || atokenExpire == null) {
+                console.error("No access token found");
                 rejected();
+                return;
             }
-        } else {
-            resolved(atoken)
+
+            const expireDate = new Date(atokenExpire);
+            const expired = new Date() > expireDate;
+            const diffMs = Math.abs(expireDate.getTime() - new Date().getTime());
+            const diffMinutes = diffMs / (1000 * 60);
+            const nearExpire = diffMinutes < 2;
+
+            if (expired || nearExpire) {
+                try {
+                    lock = true;
+                    const newToken = await refreshAccessToken();
+                    atoken = newToken.token;
+                    localStorage.setItem("atoken", newToken.token);
+                    localStorage.setItem("atokenExpire", newToken.tokenExpire);
+                    resolved(atoken);
+                    lock = false;
+                } catch (error) {
+                    console.error("Failed to get new access token: ", error)
+                    rejected();
+                }
+            } else {
+                resolved(atoken)
+            }
         }
+
+        // if a token retrieval operation is in progress wait until its comlete
+        if (lock == true) {
+            const wait = () => {
+                setTimeout(() => {
+                    if (lock == true) {
+                        return wait();
+                    } else {
+                        // no lock, return token
+                        getToken();
+                    }
+                }, 200);
+            }
+
+        } else {
+            // no locking, return token
+            getToken();
+        }
+
     })
 }
 
