d0f31842c0412c4ebee23bd64bf227bd4180aa9b
Király Sándor <kiralysandor1986@gmail.com>
2026-05-08 19:42:29 +0200

    fix masterkey setup

--- pigon-micro-web/src/lib/encryption/uploadMasterKey.ts
+++ pigon-micro-web/src/lib/encryption/uploadMasterKey.ts
@@ -1,10 +1,9 @@
 import { wrapMasterKey } from "./masterkey";
-import { BASEURL } from "../../conf";
 import api from "../../services/apiservice";
 
 const uploadMasterKey = async (masterKey: CryptoKey, kpass: string): Promise<void> => {
     const encrypted = await wrapMasterKey(masterKey, kpass);
-    await api.post(BASEURL + "/keyring/masterkey", { masterKey: encrypted })
+    await api.post("/keyring/masterkey", { masterKey: encrypted })
     return;
 }
 
