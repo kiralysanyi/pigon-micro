24670c41c430caab81eb9245ea5992ae3375030a
Király Sándor <kiralysandor1986@gmail.com>
2026-05-17 19:54:06 +0200

    fix: refactor message handling to reduce code duplication and improve readability

--- pigon-micro-web/src/services/chatservice/historyHandler.ts
+++ pigon-micro-web/src/services/chatservice/historyHandler.ts
@@ -16,6 +16,41 @@ const historyHandler = (cs: ChatService, chatID: number): Promise<Message[]> => 
             let decrypted: Message[] = [];
             let dkey: CryptoKey | undefined;
 
+            // helper function for pushing decrypted text messages to decrypted array
+            const pushTextMessage = async (msg: EncryptedMessage, dMesssage: string) => {
+                if (dkey == undefined) {
+                    console.error("dKey is undefined")
+                    return;
+                }
+
+                decrypted.push({
+                    chatID: chatID,
+                    date: new Date(msg.date),
+                    message: dMesssage,
+                    senderID: msg.senderID,
+                    type: msg.type,
+                    senderName: await getUsernameById(msg.senderID)
+                })
+            }
+
+            const pushFileMessage = async (msg: EncryptedMessage) => {
+                if (dkey == undefined) {
+                    console.error("dKey is undefined")
+                    return;
+                }
+                const assetId = JSON.parse(msg.payload).assetId;
+                decrypted.push({
+                    chatID: msg.chatID,
+                    date: new Date(msg.date),
+                    message: undefined,
+                    toLoad: assetId,
+                    senderID: msg.senderID,
+                    type: msg.type,
+                    dKey: dkey,
+                    senderName: await getUsernameById(msg.senderID)
+                })
+            }
+
             // decrypt handler
 
             // group message handler
@@ -35,31 +70,12 @@ const historyHandler = (cs: ChatService, chatID: number): Promise<Message[]> => 
 
                     dkey = await getGroupDecryptKey(chatID, msg.kGuid, cs.privKey);
                     if (msg.type == "text") {
-                        decrypted.push({
-                            chatID: chatID,
-                            date: new Date(msg.date),
-                            message: await decryptMsg(JSON.parse(msg.payload), dkey),
-                            senderID: msg.senderID,
-                            type: msg.type,
-                            senderName: await getUsernameById(msg.senderID)
-                        })
+                        await pushTextMessage(msg, await decryptMsg(JSON.parse(msg.payload), dkey));
 
                         resolve();
                     } else {
                         // handle file messages
-                        // TODO: avoid code duplication with private message handler
-                        // TODO: implement new render pipeline
-                        const assetId = JSON.parse(msg.payload).assetId;
-                        decrypted.push({
-                            chatID: msg.chatID,
-                            date: new Date(msg.date),
-                            message: undefined,
-                            toLoad: assetId,
-                            senderID: msg.senderID,
-                            type: msg.type,
-                            dKey: dkey,
-                            senderName: await getUsernameById(msg.senderID)
-                        })
+                        await pushFileMessage(msg);
                         resolve();
                     }
 
@@ -87,14 +103,7 @@ const historyHandler = (cs: ChatService, chatID: number): Promise<Message[]> => 
                     // handle text messages
                     if (msg.type == "text") {
                         decryptMsg(JSON.parse(msg.payload), dkey).then(async (message) => {
-                            decrypted.push({
-                                chatID: msg.chatID,
-                                date: new Date(msg.date),
-                                message: message,
-                                senderID: msg.senderID,
-                                type: msg.type,
-                                senderName: await getUsernameById(msg.senderID)
-                            })
+                            await pushTextMessage(msg, message);
                             resolve();
                         }).catch((err) => {
                             console.error("Failed to decrypt message: ", err)
@@ -103,20 +112,7 @@ const historyHandler = (cs: ChatService, chatID: number): Promise<Message[]> => 
                         })
                     } else {
                         // handle file messages
-                        // TODO: avoid code duplication with group message handler
-                        // TODO: implement new render pipeline
-                        const assetId = JSON.parse(msg.payload).assetId;
-
-                        decrypted.push({
-                            chatID: msg.chatID,
-                            date: new Date(msg.date),
-                            message: undefined,
-                            toLoad: assetId,
-                            senderID: msg.senderID,
-                            type: msg.type,
-                            dKey: dkey,
-                            senderName: await getUsernameById(msg.senderID)
-                        })
+                        await pushFileMessage(msg);
                         resolve();
                     }
                 })
