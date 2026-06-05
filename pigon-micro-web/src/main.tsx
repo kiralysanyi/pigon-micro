db493c2a5dfae524acb473b42280cba234f52588
Király Sándor <kiralysandor1986@gmail.com>
2026-06-05 17:00:37 +0200

    Refactor styling

--- pigon-micro-web/src/main.tsx
+++ pigon-micro-web/src/main.tsx
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import './styles/mobile.css'
 import App from './App.tsx'
 import { KeyRingProvider } from './services/KeyRingProvider.tsx'
 
