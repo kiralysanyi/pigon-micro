1629fb735e05e1a4c7e671540519381eb8fef07d
Király Sándor <kiralysandor1986@gmail.com>
2026-06-08 21:29:14 +0200

    design fix

--- pigon-micro-web/src/components/GlassButton.tsx
+++ pigon-micro-web/src/components/GlassButton.tsx
@@ -2,7 +2,7 @@ import { LiquidGlass } from "@liquidglass/react"
 
 const GlassButton = ({ children, onClick, className }: { children: any, onClick: React.MouseEventHandler<HTMLButtonElement> | undefined, className?: string }) => {
     return <button type="button" onClick={onClick} className={`${className} glassbutton-parent`} style={{ border: "none" }}>
-        <LiquidGlass className="glassbtn" saturation={5} blur={1}>
+        <LiquidGlass className="glassbtn" saturation={5} blur={1} displacementScale={2} shadowIntensity={2}>
             {children}
         </LiquidGlass>
     </button>
